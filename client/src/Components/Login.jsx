37789248d4b50db0ca95371ffb58fa76a44540c2
Palinlin <ibson.palinlin@accenture.com>
2024-02-23 14:34:04 +0800

    Initial setup of client and server side

---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import "./style.css"; // Add this line to import the style.css file
+
+const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/api/login", values)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="p-3 rounded w-25 border loginForm">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="emai">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter email..."
+              className="form-control rounded-0"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password..."
+              className="form-control rounded-0"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+            />
+          </div>
+          <button
+            className="btn btn-success w-100 rounded-0 mb-2"
+            type="submit"
+          >
+            Log in
+          </button>
+          <div className="mb-1">
+            <input type="checkbox" name="tick" id="tick" className="me-2" />
+            <label htmlFor="tick">
+              Are you agree with our terms & conditions?
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
