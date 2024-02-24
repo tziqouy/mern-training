c17c8cba5a7f30bc11d474bd334c719a8c4a29b3
kiddliam <vansfreelancer@gmail.com>
2024-02-24 16:17:11 +0800

    Add pages folder and route at dashboard file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
-import Dashboard from "./Components/Dashboard";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/dashboard" element={<Dashboard />}>
+            <Route path="" element={<Home/>}></Route>
+            <Route path="/dashboard/employee" element={<h1>Employee</h1>}></Route>
+            <Route path="/dashboard/profile" element={<h1>Profile</h1>}></Route>
+          </Route>
         </Routes>
       </Router>
     </>
